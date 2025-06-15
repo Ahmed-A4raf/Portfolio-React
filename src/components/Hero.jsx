05bcf0375f60671519635b010dc51d1598b3e6af
Ahmed Ashraf <ahmedashraf12777@gmail.com>
2025-06-15 09:45:52 +0300

    edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
 
         <div className="hidden lg:block">
             <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-40/40
-            to-65% rounded-[60px] overflow-hidden">
+            to-65% rounded-[50px] overflow-hidden">
                 <img src="/images/hero-banner.png" width={656} height={800} alt="Ahmed Ashraf" className="w-full" />
             </figure>
         </div>
